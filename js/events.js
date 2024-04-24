0c7a988e9b0941a842bf8444f4a2bf2cd50050b1
Tetiana Tarasovych <86785655+Tate-T@users.noreply.github.com>
2024-04-24 21:35:18 +0200

    .

--- js/events.js
+++ js/events.js
@@ -1,15 +1,15 @@
 // Створіть блок на сторінці, який реагує на рух миші. Кожен раз, коли користувач рухає мишкою, змінюються координати точки на графіку відповідно до руху миші. Використайте метод throttle з бібліотеки lodash для того, щоб координати оновлювалися не занадто часто та не затримували користувача. Встановіть час затримки в мілісекундах, наприклад 50 мс, і передайте функцію, яка буде виконуватися при русі миші.
 
-// const pointEl = document.querySelector('#point');
-// const coordEl = document.querySelector('#coord');
+const pointEl = document.querySelector('#point');
+const coordEl = document.querySelector('#coord');
 
-// const movePoint = (e) => {
-//     coordEl.innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`;
-//     pointEl.setAttribute('cx', e.clientX);
-//     pointEl.setAttribute('cy', e.clientY);
-// }
+const movePoint = (e) => {
+    coordEl.innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`;
+    pointEl.setAttribute('cx', e.clientX);
+    pointEl.setAttribute('cy', e.clientY);
+}
 
-// document.addEventListener('mousemove', _.throttle((e) => movePoint(e), 50));
+document.addEventListener('mousemove', _.throttle((e) => movePoint(e), 50));
 
 // 4-task
 const tech = [
